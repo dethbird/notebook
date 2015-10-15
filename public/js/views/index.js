0067fabd03fab7af4ff83e75f624f8b87ad58b0b
Rishi Satsangi <rishi.satsangi@gmail.com>
2015-10-15 18:25:31 -0400

    jquery ui sortable grid

--- public/js/views/index.js
+++ public/js/views/index.js
@@ -8,11 +8,19 @@ var notes = new NotesCollection(data);
 var NotesView = Backbone.View.extend({
     notes: notes,
     events: {
-        "click .thumbnail": "selectPanel",
-        "click #viewer": "nextPanel"
+        // "click .thumbnail": "selectPanel",
+        // "click #viewer": "nextPanel"
     },
     initialize: function(){
-        $(this.el).sortable();
+        // $(this.el).sortable({
+        //     start: function(i,e){
+        //         $(this).find('article').removeClass('animation-target');
+        //     },
+        //     stop: function(i,e){
+        //         // console.log(e.item);
+        //         e.item.addClass('animation-target');
+        //     }
+        // });
         this.render();
     },
     render: function(){
@@ -20,9 +28,18 @@ var NotesView = Backbone.View.extend({
         var that = this;
         var template = _.template($('#article-template').html());
         $.each(this.notes.models, function(i,e){
+            e.set('cols', 0 + e.get('weight'));
+            e.set('rows', 50 * e.get('weight'));
             $(that.el).append( template( { data: e.attributes }, {escape: false}) );
         });
 
+        $('section#articles').sortable({
+            items: 'article',
+            grid: [160, 50],
+            tolerance: "pointer"
+        });
+
+        // $('section#articles').gridster();
 
     }
 });
